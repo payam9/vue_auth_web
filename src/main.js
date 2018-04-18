948dfcb46cabf2e5c8ec6ce7a6263eb0db3bc36d
payam9 <payamkhosravi4@gmail.com>
2018-04-18 15:03:50 -0700

    second commit

--- src/main.js
+++ src/main.js
@@ -8,12 +8,7 @@ import firebase from 'firebase'
 
 Vue.use(Vuetify)
 firebase.initializeApp({
-  apiKey: 'AIzaSyBfsGaNRQcssSib4akIDcl4AGb_T4WcLGQ',
-  authDomain: 'vueproject-7cc4a.firebaseapp.com',
-  databaseURL: 'https://vueproject-7cc4a.firebaseio.com',
-  projectId: 'vueproject-7cc4a',
-  storageBucket: 'vueproject-7cc4a.appspot.com',
-  messagingSenderId: '370610522876'
+ 
 
 })
 Vue.config.productionTip = false
